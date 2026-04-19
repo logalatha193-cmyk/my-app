a62376c1dfb460ccb7d0c36574ad4299fad3e312
LogaPriya <logalatha193@gmail.com>
2026-04-19 20:59:45 +0530

    fix server startup for render

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("App is running!");
+});
+
+// IMPORTANT: Render needs this
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
